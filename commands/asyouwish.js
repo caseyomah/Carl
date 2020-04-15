8a30d76f3f0b90ff2cdc2e2e8abae0b90e0378d2
caseyomah <caseyomah@gmail.com>
2020-04-15 12:14:27 -0400

    Bug fixes for #as-you-wish

--- commands/asyouwish.js
+++ commands/asyouwish.js
@@ -17,14 +17,15 @@ const ext="txt"; // Extension to use for data files
 const types={"📺":["tv","show"],"🎞️":["movie","movie"],"🎵":["music","song"],"📖":["audiobook","audiobook"],"📖":["book","book"],"🎲":["rpg","role-playing game"],"💥":["comic","comic book"]};
 var log={};
 Object.values(types).forEach(key=>log[key[0]]=CSV.readArraySync(filepath+key[0]+"."+ext));
-watchReacts=function(m,f,l,k) {
-    const filter = (reaction) => true||reaction.emoji.name === '✨';m.createReactionCollector(filter).on('collect', (r,c) => {
+watchReacts=function(m,f,l,k,cc) {
+    const filter=(reaction,user)=>reaction.emoji.name==='✨'; //&&(user.roles.has("581334517151825920")||user.roles.has("581538686265589772"));
+    m.createReactionCollector(filter).on('collect', (r,c) => {
         t=[];
         log[f].forEach((v,i)=>{if (i!==k) t.push(v)});
         log[f]=t;
-        let pings="<@"+m.author+">";
-        if (m.reactions.has("☝️")) m.reactions.get("☝️").users.forEach(u=>pings+="<@"+u.id+">");
-        chatchan.send(pings+l[2]+" is "+(err?"fixed":"up")+".");
+        let pings="<@"+m.author.id+">";
+        if (m.reactions.has("☝️")) m.reactions.get("☝️").users.forEach(u=>pings+=" <@"+u.id+">");
+        cc.send(pings+l[2]+" is "+(err?"fixed":"up")+".");
         CSV.writeArraySync(filepath+f+"."+ext,log[f]);
         c.stop();
     })
@@ -49,9 +50,9 @@ module.exports=function(message) {
     let deleteMsg=true,type=undefined,dmText="I'm sorry, this channel is reserved for "+mode+"s, please take conversations to "+chatchan+".";
 if (!reacts) {
     reacts=true;
-    Object.keys(log).forEach(f=>log[f].forEach((l,k)=>chan.fetchMessage(l[1]).then(m=>watchReacts(m,f,l,k)).catch()));
+    Object.keys(log).forEach(f=>log[f].forEach((l,k)=>chan.fetchMessage(l[1]).then(m=>watchReacts(m,f,l,k,chatchan)).catch()));
 }
-    if (message.channel===chan) {
+    if (message.channel==chan) {
         info=message.content.match(err?/^(🛑)?\s*(\S+)\s+\*\*(.*)\*\*\s+\((.*)\)\s*-?\s*S?(\d{2})?E?(\d{2})?(\s+\*(.*)\*)?$/:/^(🛑)?\s*(\S+)\s+\*\*(.*)\*\*\s+\((.*)\)(\s+on\s+(.+))?(\s+\*(.*)\*)?$/);
         if (info && info.length==9&&info[2]&&!err==!info[1]) {
             switch(info[2]) {
@@ -59,42 +60,33 @@ if (!reacts) {
                     //:tv:   **TITLE** (DATE, STATUS [Upcoming, Ongoing, Ended]) on CHANNEL *OPTIONAL NOTES TO HELP DIFFERENTIATE*
                     type=types[info[2]];
                     status=["Unknown","Upcoming","Ongoing","Ended"];
-                    if (!err) {
-                        date=info[4].match(/^(\S{4}), (.*)$/);
-                        if (!date) dmText="I'm sorry, I didn't understand your date and/or status.";
-                        else if (!status.includes(date[2])) dmText="I'm sorry, that is not a valid status.\n\nValid statuses are: "+status.slice(1).join(", ")+".";
-                    }
-                    else if (!info[4].match(/^\S{4}$/)) dmText="I'm sorry, when was that "+type[1]+" released?";
-                    else if (err) {
-                        if (!info[5]) dmText="I'm sorry, what season of that "+type[1]+" again?";
-                        else if (!info[6]) dmText="I'm sorry, what episode of that "+type[1]+" again?";
-                        else if (!info[8]) dmText="I'm sorry, what was wrong with this "+type[1]+"?";
-                    }
+                    date=info[4].match(/^(.+), *(.*)$/);
+                    if (!err&&!date) dmText="I'm sorry, I didn't understand your date and/or status.";
+                    else if (!err&&!status.includes(date[2])) dmText="I'm sorry, that is not a valid status.\n\nValid statuses are: "+status.join(/, */)+".";
+                    else if (err&&!info[5]) dmText="I'm sorry, what season of that "+type[1]+" again?";
+                    else if (err&&!info[6]) dmText="I'm sorry, what episode of that "+type[1]+" again?";
+                    else if (err&&!info[8]) dmText="I'm sorry, what was wrong with this "+type[1]+"?";
                     else if (!info[6]) dmText="I'm sorry, what channel is that "+type[1]+" on again?";
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+(err?"":"`"+date[2].toLowerCase()+"` ")+type[1]+" `"+info[3]+"` from "+(err?"episode ":"")+"`"+info[6]+"` "+(err?"of season `"+info[5]+"` ":"")+"premiering in `"+(err?info[4]:date[1])+"`."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        if (err) log[type[0]].push(message.author.id,message.id,info[3],info[4],info[5],info[6],info[8]);
-                        else log[type[0]].push(message.author.id,message.id,info[3],date[1],date[2],info[6],(info[8]?info[8]:""));
+                        if (err) log[type[0]].push([message.author.id,message.id,info[3],info[4],info[5],info[6],info[8]]);
+                        else log[type[0]].push([message.author.id,message.id,info[3],date[1],date[2],info[6],(info[8]?info[8]:"")]);
                     }
                     break;
                 case "🎞️":
                     //:film_frames:  **TITLE** (DATE, STATUS [Upcoming, TV Special, In Theater, DVD Release]) *OPTIONAL NOTES TO HELP DIFFERENTIATE*
                     type=types[info[2]];
                     status=["Unknown","Upcoming","TV Special","In Theater","DVD Release"];
-                    if (!err) {
-                        date=info[4].match(/^(\S{4}), (.*)$/);
-                        if (!date) dmText="I'm sorry, I didn't understand your date and/or status.";
-                        else if (!status.includes(date[2])) dmText="I'm sorry, that is not a valid status.\n\nValid statuses are: "+status.slice(1).join(", ")+".";
-                        else if (!date[1].match(/^\S{4}$/)) dmText="I'm sorry, when was that "+type[1]+" released?";
-                    }
-                    else if (!info[4].match(/^\S{4}$/)) dmText="I'm sorry, when was that "+type[1]+" released?";
-                    else if (!info[8]) dmText="I'm sorry, what was wrong with this "+type[1]+"?";
+                    date=info[4].match(/^(.+), *(.*)$/);
+                    if (!err&&!date) dmText="I'm sorry, I didn't understand your date and/or status.";
+                    else if (!err&&!status.includes(date[2])) dmText="I'm sorry, that is not a valid status.\n\nValid statuses are: "+status.join(", ")+".";
+                    else if (err&&!info[8]) dmText="I'm sorry, what was wrong with this "+type[1]+"?";
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+(err?"":"`"+date[2].toLowerCase()+"` ")+type[1]+" `"+info[3]+"` premiering in `"+(err?info[4]:date[1])+"`."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        if (err) log[type[0]].push(message.author.id,message.id,info[3],info[4],info[8]);
-                        else log[type[0]].push(message.author.id,message.id,info[3],date[1],date[2],(info[8]?info[8]:""));
+                        if (err) log[type[0]].push([message.author.id,message.id,info[3],info[4],info[8]]);
+                        else log[type[0]].push([message.author.id,message.id,info[3],date[1],date[2],(info[8]?info[8]:"")]);
                     }
                     break;
                 case "🎵":
@@ -110,7 +102,7 @@ if (!reacts) {
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+type[1]+" `"+tags[1]+"` as performed by `"+tags[0]+"` in `"+info[4]+"`."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        log[type[0]].push(message.author.id,message.id,tags[1],tags[2],info[4],(info[8]?info[8]:""));
+                        log[type[0]].push([message.author.id,message.id,tags[1],tags[2],info[4],(info[8]?info[8]:"")]);
                     }
                     break;
                 case "📖":
@@ -126,7 +118,7 @@ if (!reacts) {
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+type[1]+" `"+tags[2]+"` written by `"+tags[0]+"` in `"+info[4]+"` part of the `"+tags[1]+"` series."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        log[type[0]].push(message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:""));
+                        log[type[0]].push([message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:"")]);
                     }
                     break;
                 case "📚":
@@ -142,7 +134,7 @@ if (!reacts) {
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+type[1]+" `"+tags[2]+"` written by `"+tags[0]+"` in `"+info[4]+"` part of the `"+tags[1]+"` series."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        log[type[0]].push(message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:""));
+                        log[type[0]].push([message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:"")]);
                     }
                     break;
                 case "🎲":
@@ -158,7 +150,7 @@ if (!reacts) {
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+type[1]+" `"+tags[2]+"` published by `"+tags[0]+"` in `"+info[4]+"` part of the `"+tags[1]+"` series."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        log[type[0]].push(message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:""));
+                        log[type[0]].push([message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:"")]);
                     }
                     break;
                 case "💥":
@@ -174,7 +166,7 @@ if (!reacts) {
                     else {
                         deleteMsg=false;
                         dmText="Thank you for "+mode+"ing the "+type[1]+" `"+tags[2]+"` published by `"+tags[0]+"` in `"+info[4]+"` part of the `"+tags[1]+"` series."+(!info[8]?"":" The following notes have been included: `"+info[8]+"`.");
-                        log[type[0]].push(message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:""));
+                        log[type[0]].push([message.author.id,message.id,tags[1],tags[2],tags[3],info[4],(info[8]?info[8]:"")]);
                     }
                     break;
                 default:
@@ -187,8 +179,8 @@ if (!reacts) {
             message.delete(1).catch();
         }
         else {
-            CSV.writeArraySync(filepath+type[0]+"."+ext,log[type[0]].slice(-1));
-            watchReacts(message,type[0],log[type[0]].slice(-1),(log[type[0]].length-1));
+            CSV.writeArraySync(filepath+type[0]+"."+ext,log[type[0]]);
+            watchReacts(message,type[0],log[type[0]][(log[type[0]].length-1)],(log[type[0]].length-1),chatchan);
         }
     }
 }
