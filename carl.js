8020a971a865bc2f0368e9732e0b5b318ae124a7
caseyomah <caseyomah@gmail.com>
2020-04-19 21:05:25 -0400

    Moved tip command to module

--- carl.js
+++ carl.js
@@ -93,94 +93,71 @@ client.on('message', msg => {
             }
         }
         //Plain text social responses
-        if (input.match(/^h(e(llo)?|i|y)a?.* carl.*/)) {
-            var say=new Array("Hello, "+Mbr(msg.member,0)+", is there something I can help you with?");
-            msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-        }
-        if (input.match(/^(good ?)?(bye|n(ight|ite)).* carl.*/)) {
-            var say=new Array("Hope to see you again soon.","Hope to see you again soon, "+Mbr(msg.member,0)+".");
-            msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-        }
-        if (input.match(/morning.* carl.*/)) {
-            var say=new Array("Great to see you again.","Great to see you again, "+Mbr(msg.member,0)+".");
-            msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-        }
-        if (input.match(/thank(s.*| ?you.*) carl.*/)) {
-            var say=new Array("It is a pleasure to be of service.","You are most welcome.");
-            msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-        }
-        
-        // Bot banter
-        if (input=="sorry, i was doing some uhh... nerdy stuff." && Math.floor(Math.random() * 10)==0) {
-            var say=new Array("Oh my!","I'm sure I don't want to know.","You don't say...","My goodness.","Was that Star Trek or Star Wars?");
-            msg.channel.send(say[Math.floor(Math.random()*say.length)]);
-        }
-        //// Programatic triggers
-        // emote
-        if (input.match(/^!emote/)) {
-            var em=input.slice(7);
-            if (em.length>0) {
-                em=em.split(" ");
-                for (var a=0;a<em.length;a++) {
-                    msg.channel.send("Emote:"+em[a]+"="+Em.find(em[a]));
+        else {
+            if (input.match(/^h(e(llo)?|i|y)a?.* carl.*/)) {
+                var say=new Array("Hello, "+Mbr(msg.member,0)+", is there something I can help you with?");
+                msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+            }
+            if (input.match(/^(good ?)?(bye|n(ight|ite)).* carl.*/)) {
+                var say=new Array("Hope to see you again soon.","Hope to see you again soon, "+Mbr(msg.member,0)+".");
+                msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+            }
+            if (input.match(/morning.* carl.*/)) {
+                var say=new Array("Great to see you again.","Great to see you again, "+Mbr(msg.member,0)+".");
+                msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+            }
+            if (input.match(/thank(s.*| ?you.*) carl.*/)) {
+                var say=new Array("It is a pleasure to be of service.","You are most welcome.");
+                msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+            }
+            
+            // Bot banter
+            if (input=="sorry, i was doing some uhh... nerdy stuff." && Math.floor(Math.random() * 10)==0) {
+                var say=new Array("Oh my!","I'm sure I don't want to know.","You don't say...","My goodness.","Was that Star Trek or Star Wars?");
+                msg.channel.send(say[Math.floor(Math.random()*say.length)]);
+            }
+            //// Programatic triggers
+            // emote
+            if (input.match(/^!emote/)) {
+                var em=input.slice(7);
+                if (em.length>0) {
+                    em=em.split(" ");
+                    for (var a=0;a<em.length;a++) {
+                        msg.channel.send("Emote:"+em[a]+"="+Em.find(em[a]));
+                    }
                 }
             }
-        }
 
-        // New Member follow-up
-        if (input.match(/^\"?i understand.?\"?$/) && msg.channel == newconn) {
-            newconn.send("Done? Great! Sorry to put you through that mess, but it was pretty important. Now, I'll slip a note to our "+CastingRef+" department. They should be by soon to answer any questions and let you in.");
-            setTimeout(function() {newconn.send("Oh, I almost forgot! Once you're in, if you need help, be sure to ask in the "+HelpRef+" channel. You can also type !help to see what I can help you with.")},5000);
-        }
-        
-        // recs reply
-        /*
-        if (args=input.match(/^!recs? ?(.*)?/)) {
-            if (args[1]) {
-                say= (Recs.get(args[1]));
+            // New Member follow-up
+            if (input.match(/^\"?i understand.?\"?$/) && msg.channel == newconn) {
+                newconn.send("Done? Great! Sorry to put you through that mess, but it was pretty important. Now, I'll slip a note to our "+CastingRef+" department. They should be by soon to answer any questions and let you in.");
+                setTimeout(function() {newconn.send("Oh, I almost forgot! Once you're in, if you need help, be sure to ask in the "+HelpRef+" channel. You can also type !help to see what I can help you with.")},5000);
             }
-            else {
-                say= (Recs.getRandom());
+            
+            // recs reply
+            /*
+            if (args=input.match(/^!recs? ?(.*)?/)) {
+                if (args[1]) {
+                    say= (Recs.get(args[1]));
+                }
+                else {
+                    say= (Recs.getRandom());
+                }
+                msg.reply(new Discord.RichEmbed()
+                .setColor('#FFAA00')
+                .setDescription(say));
+                say=undefined;
             }
-            msg.reply(new Discord.RichEmbed()
-			.setColor('#FFAA00')
-			.setDescription(say));
-            say=undefined;
-        }
-        
-        
-        */
-        //tips reply
-        if (input.match(/^!tip/)) {
-            var say=new Array(
-                "📺 🎞️ Did you know? you can get access to the video library by sending a DM to Vaesse that includes your Plex email address, and a request for access.",
-                "📚 🎲 💥 Did you know? You can get access to our library of E-Books by requesting access to Calibre in the "+HelpRef+" channel!",
-                "Need FTP access? You can request it in the "+HelpRef+" channel!",
-                "📖 Looking for audiobooks? Check in the Audiobooks library! If you don't see it, check under the Music library. Still can't find it? Ask for help in the "+HelpRef+" channel, and someone will assist you soon!",
-                "Having technical issues, or something is not working as expected? Ask for assistance in the "+HelpRef+" channel, and one of our volunteer Tech Support reps will get back to you soon!",
-                "Have a show, movie, album, or book you want to recommend to everyone?  Let us know what you love and why in the "+HelpRef+" channel, and we'll add a tip!"
-            );
-            var embed = new Discord.RichEmbed()
-			.setColor('#00AA00')
-			.setDescription(say[Math.floor(Math.random()*say.length)]);
-            msg.channel.send({ embed });
-        }
-        // "🇹 🇮 🇵 Have you seen the show **Nikita**? Vaesse recommends it saying, \"This reboot of 'La Femme Nikita' is an action-thriller. All the characters are amazing!\" Check it out in the TV library!"
-        
-        /* Unicode Symbols for various services
-        *	 📺   TV
-        *	 🎞️  Movies
-        *	 🎵   Music
-        *	 📖   Audiobook
-        *	 📚   Book
-        *	 🎲   RPG
-        *	 💥   Comics
-        */
-        
-        
-        // help text
-        if (input.match(/^!help/)||input.match(/^help.*carl.*/)) {
-            msg.channel.send(msg.author+', here\'s a quick help list!\n\n!ping ["plex"/"calibre"/"ftp"/"all"/""] - Asks me the status of various services.\n!tips - Asks me for a random tip.\n!help - Tells me to display this message.\n\nIf you need assistance or have a suggestion for my service, let a member of our Casting staff know in '+HelpRef+'.');
+            
+            
+            */
+            //tips reply
+            
+            
+            // help text
+            if (input.match(/^!help/)||input.match(/^help.*carl.*/)) {
+                msg.channel.send(msg.author+', here\'s a quick help list!\n\n!ping ["plex"/"calibre"/"ftp"/"all"/""] - Asks me the status of various services.\n!tips - Asks me for a random tip.\n!help - Tells me to display this message.\n\nIf you need assistance or have a suggestion for my service, let a member of our Casting staff know in '+HelpRef+'.');
+            }
         }
     }
 });
---
+++ commands/tip.js
@@ -0,0 +1,31 @@
+modules.export={
+    name:"tip",
+    description:"Request a tip",
+    rich:{
+        color:0x00AA00,
+        title:"tip"
+    },
+    execute(msg,args) {
+                var say=new Array(
+                    "📺 🎞️ Did you know? you can get access to the video library by sending a DM to Vaesse that includes your Plex email address, and a request for access.",
+                    "📚 🎲 💥 Did you know? You can get access to our library of E-Books by requesting access to Calibre in the "+HelpRef+" channel!",
+                    "Need FTP access? You can request it in the "+HelpRef+" channel!",
+                    "📖 Looking for audiobooks? Check in the Audiobooks library! If you don't see it, check under the Music library. Still can't find it? Ask for help in the "+HelpRef+" channel, and someone will assist you soon!",
+                    "Having technical issues, or something is not working as expected? Ask for assistance in the "+HelpRef+" channel, and one of our volunteer Tech Support reps will get back to you soon!",
+                    "Have a show, movie, album, or book you want to recommend to everyone?  Let us know what you love and why in the "+HelpRef+" channel, and we'll add a tip!"
+                );
+                var embed(()=>{return this.rich})();                embed.description=say[Math.floor(Math.random()*say.length)];
+                msg.channel.send({ embed });
+            }
+            // "🇹 🇮 🇵 Have you seen the show **Nikita**? Vaesse recommends it saying, \"This reboot of 'La Femme Nikita' is an action-thriller. All the characters are amazing!\" Check it out in the TV library!"
+            
+            /* Unicode Symbols for various services
+            *	 📺   TV
+            *	 🎞️  Movies
+            *	 🎵   Music
+            *	 📖   Audiobook
+            *	 📚   Book
+            *	 🎲   RPG
+            *	 💥   Comics
+            */
+}
